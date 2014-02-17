c7030877280b703b1096c4f18d898ec59a831393
Kazuya Takeshima <mail@mitukiii.jp>
2014-02-18 04:02:28 +0900

    Add specs

---
+++ spec/cha/client_spec.rb
@@ -0,0 +1,184 @@
+# coding: utf-8
+
+require 'spec_helper'
+
+describe Cha::Client do
+  context :api do
+    let(:api_token) { 'api_token' }
+    let(:client) { Cha.client(api_token: api_token) }
+
+    describe '#me' do
+      it 'should request the correct resource' do
+        stub_get('me')
+        client.me
+      end
+    end
+
+    describe '#my_status' do
+      it 'should request the correct resource' do
+        stub_get('my/status')
+        client.my_status
+      end
+    end
+
+    describe '#my_tasks' do
+      it 'should request the correct resource' do
+        stub_get('my/tasks')
+        client.my_tasks
+      end
+    end
+
+    describe '#contacts' do
+      it 'should request the correct resource' do
+        stub_get('contacts')
+        client.contacts
+      end
+    end
+
+    describe '#rooms' do
+      it 'should request the correct resource' do
+        stub_get('rooms')
+        client.rooms
+      end
+    end
+
+    describe '#create_room' do
+      pending
+    end
+
+    describe '#room' do
+      it 'should request the correct resource' do
+        stub_get('rooms/1')
+        client.room(1)
+      end
+    end
+
+    describe '#update_room' do
+      pending
+    end
+
+    describe '#destroy_room' do
+      pending
+    end
+
+    describe '#room_members' do
+      it 'should request the correct resource' do
+        stub_get('rooms/1/members')
+        client.room_members(1)
+      end
+    end
+
+    describe '#update_room_members' do
+      pending
+    end
+
+    describe '#room_messages' do
+      it 'should request the correct resource' do
+        stub_get('rooms/1/messages')
+        client.room_messages(1)
+      end
+    end
+
+    describe '#create_room_message' do
+      pending
+    end
+
+    describe '#room_message' do
+      pending
+    end
+
+    describe '#room_tasks' do
+      it 'should request the correct resource' do
+        stub_get('rooms/1/tasks')
+        client.room_tasks(1)
+      end
+    end
+
+    describe '#create_room_task' do
+      pending
+    end
+
+    describe '#room_task' do
+      it 'should request the correct resource' do
+        stub_get('rooms/1/tasks/1')
+        client.room_task(1, 1)
+      end
+    end
+
+    describe '#room_files' do
+      it 'should request the correct resource' do
+        stub_get('rooms/1/files')
+        client.room_files(1)
+      end
+    end
+
+    describe '#room_file' do
+      it 'should request the correct resource' do
+        stub_get('rooms/1/files/1')
+        client.room_file(1, 1)
+      end
+    end
+  end
+
+  context 'with module configuration' do
+    let(:keys) { Cha::Configuration::VALID_OPTIONS_KEYS }
+
+    before do
+      Cha.configure do |config|
+        keys.each do |key|
+          config.send("#{key}=", key)
+        end
+      end
+    end
+
+    after do
+      Cha.reset
+    end
+
+    it 'should inherit module configuration' do
+      client = Cha::Client.new
+      keys.each do |key|
+        expect(client.send(key)).to eql(key)
+      end
+    end
+
+    context 'with class configuration' do
+      let(:configuration) do
+        {
+          adapter: :typhoeus,
+          api_token: 'AT',
+          endpoint: 'https://api.twitter.com',
+          proxy: 'http://mitukiii:secret@proxy.example.com:8080',
+          user_agent: 'Custom User Agent'
+        }
+      end
+
+      context 'during initialization' do
+        it 'should override module configuration' do
+          client = Cha::Client.new(configuration)
+          keys.each do |key|
+            expect(client.send(key)).to eql(configuration[key])
+          end
+        end
+      end
+
+      context 'after initilization' do
+        it 'should override module configuration after initialization' do
+          client = Cha::Client.new
+          configuration.each do |key, value|
+            client.send("#{key}=", value)
+          end
+          keys.each do |key|
+            expect(client.send(key)).to eql(configuration[key])
+          end
+        end
+      end
+    end
+  end
+
+  context 'when have error' do
+    let(:client) { Cha.new }
+
+    pending
+  end
+end
---
+++ spec/cha_spec.rb
@@ -0,0 +1,86 @@
+# coding: utf-8
+
+require 'spec_helper'
+
+describe Cha do
+  after do
+    Cha.reset
+  end
+
+  context 'when delegating to a client' do
+    before do
+      stub_get('me')
+        .to_return(body: '{"nyan":"にゃーん"}')
+    end
+
+    it 'should request the correct resource' do
+      Cha.me
+    end
+
+    it 'should return the same results as a client' do
+      expect(Cha.me).to eql(Cha::Client.new.me)
+    end
+  end
+
+  describe '.client' do
+    it 'should return a Cha::Client' do
+      expect(Cha.client).to be_is_a(Cha::Client)
+    end
+  end
+
+  describe '.respond_to?' do
+    it 'should take an optional argument' do
+      expect(Cha.respond_to?(:client, true)).to be_true
+    end
+  end
+
+  describe '.adapter' do
+    it 'should return the default adapter' do
+      expect(Cha.adapter).to eql(Cha::Configuration::DEFAULT_ADAPTER)
+    end
+  end
+
+  describe '.adapter=' do
+    it 'should set the adapter' do
+      Cha.adapter = :typhoeus
+      expect(Cha.adapter).to eql(:typhoeus)
+    end
+  end
+
+  describe '.endpoint' do
+    it 'should return the default endpoint' do
+      expect(Cha.endpoint).to eql(Cha::Configuration::DEFAULT_ENDPOINT)
+    end
+  end
+
+  describe '.endpoint=' do
+    it 'should set the endpoint' do
+      Cha.endpoint = 'https://api.twitter.com/'
+      expect(Cha.endpoint).to eql('https://api.twitter.com/')
+    end
+  end
+
+  describe '.user_agent' do
+    it 'should return the default user agent' do
+      expect(Cha.user_agent).to eql(Cha::Configuration::DEFAULT_USER_AGENT)
+    end
+  end
+
+  describe '.user_agent=' do
+    it 'should set the user_agent' do
+      Cha.endpoint = 'Custom User Agent'
+      expect(Cha.endpoint).to eql('Custom User Agent')
+    end
+  end
+
+  describe '.configure' do
+    Cha::Configuration::VALID_OPTIONS_KEYS.each do |key|
+      it "should set the #{key}" do
+        Cha.configure do |config|
+          config.send("#{key}=", key)
+          expect(Cha.send(key)).to eql(key)
+        end
+      end
+    end
+  end
+end
